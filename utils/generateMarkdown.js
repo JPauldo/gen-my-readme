8bf1f54c2931f45a103e88f8d8dc82c34df1e484
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2022-12-03 20:49:14 -0500

    Created README template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,47 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+![ [Insert Badge Here] ](https://img.shields.io/badge/Badge-Placeholder-brightgreen)
+
+## Description
+
+[Insert Description Info Here]
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Tests](#tests)
+- [How to Contribute](#how-to-contribute)
+
+## Installation
+
+If you would like to run this application, please run the following command(s) to install the necessary requirements:
+\`\`\`
+\`\`\`
+
+## Usage
+
+Below is a demonstration of the application: 
+
+![Alt Text for Placeholder](https://via.placeholder.com/150)
+
+## License
+
+[Insert License Info Here]
+
+## Tests
+
+If you would like to run tests for this repo, please run the following command(s):
+\`\`\`
+\`\`\`
+
+## How to Contribute
+
+If you would like to contribute, feel free to reach out to me at [alias@email.com](mailto:alias@email.com).
+
+If you like what you see here, please feel free to checkout my other work on [my Github page](https://github.com/).
 `;
 }
 
